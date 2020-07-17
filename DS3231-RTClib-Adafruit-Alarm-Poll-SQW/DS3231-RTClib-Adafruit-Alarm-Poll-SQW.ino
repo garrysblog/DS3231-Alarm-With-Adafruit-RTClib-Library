c199a9a0462cf663faeb04fb2ab9e44efb910d43
Garry <62373139+garrysblog@users.noreply.github.com>
2020-07-17 18:14:27 +1000

    Update DS3231-RTClib-Adafruit-Alarm-Poll-SQW.ino

--- DS3231-RTClib-Adafruit-Alarm-Poll-SQW/DS3231-RTClib-Adafruit-Alarm-Poll-SQW.ino
+++ DS3231-RTClib-Adafruit-Alarm-Poll-SQW/DS3231-RTClib-Adafruit-Alarm-Poll-SQW.ino
@@ -31,6 +31,12 @@ void setup () {
   Serial.begin(9600); // Start serial port for monitoring
 
   pinMode(alarmPin, INPUT_PULLUP); // Set alarm pin as pullup
+  
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
 
   // If required set to to compile time
   //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
